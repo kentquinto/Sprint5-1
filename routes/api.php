a3a48fe5557ed9cc6972e462dbb472b1af250ce9
Kent Quinto <kentquinto1@gmail.com>
2026-06-09 10:07:37 +0200

    added api routes to finish implementing endpoints for eventsread GREEN

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,13 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\EventController;
 use App\Http\Controllers\Api\GameController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/games', [GameController::class, 'index']);
+Route::get('/games',        [GameController::class,  'index']);
+Route::get('/events',       [EventController::class, 'index']);
+Route::get('/events/{event}', [EventController::class, 'show']);
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login',    [AuthController::class, 'login']);
