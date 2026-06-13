2fe27e22398f464b2b36772fba3e767dba84df49
Kent Quinto <kentquinto1@gmail.com>
2026-06-13 11:52:31 +0200

    added participant api routes to finally implement participants endpoints GREEN

--- routes/api.php
+++ routes/api.php
@@ -3,16 +3,20 @@
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\EventController;
 use App\Http\Controllers\Api\GameController;
+use App\Http\Controllers\Api\ParticipantController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/games',        [GameController::class,  'index']);
 Route::get('/events',           [EventController::class, 'index']);
 Route::get('/events/{event}',   [EventController::class, 'show']);
+Route::get('/events/{event}/participants', [ParticipantController::class, 'index']);
 
 Route::middleware('auth:api')->group(function () {
-    Route::post('/events',             [EventController::class, 'store']);
-    Route::put('/events/{event}',      [EventController::class, 'update']);
-    Route::delete('/events/{event}',   [EventController::class, 'destroy']);
+    Route::post('/events',                    [EventController::class,     'store']);
+    Route::put('/events/{event}',             [EventController::class,     'update']);
+    Route::delete('/events/{event}',          [EventController::class,     'destroy']);
+    Route::post('/events/{event}/participants',   [ParticipantController::class, 'store']);
+    Route::delete('/events/{event}/participants', [ParticipantController::class, 'destroy']);
 });
 
 Route::post('/register', [AuthController::class, 'register']);
