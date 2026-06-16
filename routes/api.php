f8d776fb3bcb10a72a2de091bd757980a9333fb5
Kent Quinto <kentquinto1@gmail.com>
2026-06-16 13:51:38 +0200

    implemented statistics endpoints in route api GREEN

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Api\EventController;
 use App\Http\Controllers\Api\GameController;
 use App\Http\Controllers\Api\ParticipantController;
 use App\Http\Controllers\Api\PlayerController;
+use App\Http\Controllers\Api\StatisticsController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/games',        [GameController::class,  'index']);
@@ -12,6 +13,9 @@ Route::get('/events',           [EventController::class, 'index']);
 Route::get('/events/{event}',   [EventController::class, 'show']);
 Route::get('/events/{event}/participants', [ParticipantController::class, 'index']);
 Route::get('/players/{user}',             [PlayerController::class,      'show']);
+Route::get('/stats/players',              [StatisticsController::class,  'players']);
+Route::get('/stats/games',               [StatisticsController::class,  'games']);
+Route::get('/stats/organizers',          [StatisticsController::class,  'organizers']);
 
 Route::middleware('auth:api')->group(function () {
     Route::post('/events',                    [EventController::class,     'store']);
