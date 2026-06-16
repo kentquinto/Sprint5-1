2b06d641c5aa321bea2f106ddec88656c918b8d9
Kent Quinto <kentquinto1@gmail.com>
2026-06-16 18:33:34 +0200

    add minimum password length on register

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -18,7 +18,7 @@ class AuthController extends Controller
         $request->validate([
             'name'     => 'required|string|max:255',
             'email'    => 'required|email|unique:users',
-            'password' => 'required|confirmed',
+            'password' => 'required|confirmed|min:8',
         ]);
 
         $user = User::create($request->only(['name', 'email', 'password']));
