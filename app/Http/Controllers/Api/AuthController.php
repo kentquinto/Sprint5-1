f76bc1fcf353b8a44ee995cdff7f32bab064a670
Kent Quinto <kentquinto1@gmail.com>
2026-06-17 13:17:29 +0200

    fix: add minimum name length on register

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -14,7 +14,7 @@ class AuthController extends Controller
     public function register(Request $request): JsonResponse
     {
         $request->validate([
-            'name'     => 'required|string|max:255',
+            'name'     => 'required|string|min:2|max:255',
             'email'    => 'required|email|unique:users',
             'password' => 'required|confirmed|min:8',
         ]);
