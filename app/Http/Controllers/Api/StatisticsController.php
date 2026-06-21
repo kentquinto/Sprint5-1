91ffdc838fb61351d804d78a976e10ebd762ccb1
Kent Quinto <kentquinto1@gmail.com>
2026-06-22 00:02:11 +0200

    add scribe docblock to statistics controller

--- app/Http/Controllers/Api/StatisticsController.php
+++ app/Http/Controllers/Api/StatisticsController.php
@@ -9,6 +9,19 @@ use Illuminate\Http\JsonResponse;
 
 class StatisticsController extends Controller
 {
+    /**
+     * Top players leaderboard.
+     *
+     * Returns the top 10 players ranked by number of events joined. Only players who have joined at least one event are included.
+     *
+     * @group Statistics
+     * @unauthenticated
+     *
+     * @response 200 [
+     *   { "id": 3, "name": "Player One", "joined_events_count": 15 },
+     *   { "id": 1, "name": "Player One", "joined_events_count": 12 }
+     * ]
+     */
     public function players(): JsonResponse
     {
         $players = User::withCount('participatingEvents')
@@ -25,6 +38,20 @@ class StatisticsController extends Controller
         return response()->json($players);
     }
 
+    /**
+     * Games leaderboard.
+     *
+     * Returns all supported games ranked by how many events have been created for them.
+     *
+     * @group Statistics
+     * @unauthenticated
+     *
+     * @response 200 [
+     *   { "id": 1, "name": "Pokémon",             "events_count": 8 },
+     *   { "id": 2, "name": "Yu-Gi-Oh!",           "events_count": 5 },
+     *   { "id": 8, "name": "Magic: The Gathering", "events_count": 3 }
+     * ]
+     */
     public function games(): JsonResponse
     {
         $games = Game::withCount('events')
@@ -39,6 +66,19 @@ class StatisticsController extends Controller
         return response()->json($games);
     }
 
+    /**
+     * Top organizers leaderboard.
+     *
+     * Returns the top 10 organizers ranked by number of events created. Only organizers who have created at least one event are included.
+     *
+     * @group Statistics
+     * @unauthenticated
+     *
+     * @response 200 [
+     *   { "id": 1, "name": "Player One", "organized_events_count": 5 },
+     *   { "id": 4, "name": "Player Three", "organized_events_count": 3 }
+     * ]
+     */
     public function organizers(): JsonResponse
     {
         $organizers = User::withCount('createdEvents')
